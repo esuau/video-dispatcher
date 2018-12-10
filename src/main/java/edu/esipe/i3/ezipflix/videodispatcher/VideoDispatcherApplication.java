91ce6c501d0ce21e75940103c2f972ad5f435902
esuau <evan.suau@etu.u-pec.fr>
2018-12-10 21:19:09 +0100

    Initial commit

---
+++ src/main/java/edu/esipe/i3/ezipflix/videodispatcher/VideoDispatcherApplication.java
@@ -0,0 +1,12 @@
+package edu.esipe.i3.ezipflix.videodispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoDispatcherApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/esipe/i3/ezipflix/videodispatcher/VideoDispatcherApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.esipe.i3.ezipflix.videodispatcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoDispatcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
